e5d174d53a82f82b3ee14224573b857854d934fb
Brevan Bender <bcaybender@gmail.com>
2021-10-22 14:47:38 -0700

    added chart and formatted results

--- results/results.js
+++ results/results.js
@@ -48,4 +48,47 @@ for (let item of pokedex){
     mainContainer.append(div);
     mainPage.append(mainContainer);
 }
+const name = pokedex.map((item)=> {
+    const caughtPokemon = findById(item.id, pokemons);
+    return caughtPokemon.pokemon;
+});
 
+const caught = pokedex.map(item=>item.caught);
+
+
+var ctx = document.getElementById('resultsChart').getContext('2d');
+// eslint-disable-next-line no-undef
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: name,
+        datasets: [{
+            label: 'Times Caught',
+            data: caught,
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 5
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
